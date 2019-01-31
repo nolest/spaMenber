a87cc1987a10306f8ab8f817e5acb3b3e14ccf27
nolest <234722572@qq.com>
2019-01-31 14:38:27 +0800

    Add files via upload

---
+++ config/prod.env.js
@@ -0,0 +1,5 @@
+'use strict'
+module.exports = {
+  NODE_ENV: '"production"',
+  BASE_API: '"https://www.easy-mock.com/mock/5c171a66c3b1105b12036b38/spa"',
+}
---
+++ dist/static/js/b4on.27837f7b.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([["b4on"],{b4on:function(n,e,t){"use strict";t.r(e);var o=t("KHd+"),i=Object(o.a)({},function(){var n=this.$createElement;return(this._self._c||n)("div",[this._v("忘记")])},[],!1,null,null,null);i.options.__file="forgotSide.vue";e.default=i.exports}}]);
